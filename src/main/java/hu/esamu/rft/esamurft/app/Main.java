85d123f0770841312481ec8ff992a5bd30bfd615
Nagykéri Bence <nuttyka@gmail.com>
2016-11-18 03:39:04 +0100

    Update

--- src/main/java/hu/esamu/rft/esamurft/app/Main.java
+++ src/main/java/hu/esamu/rft/esamurft/app/Main.java
@@ -1,48 +1,113 @@
 package hu.esamu.rft.esamurft.app;
 
 import com.google.protobuf.ByteString;
+import com.google.protobuf.Timestamp;
 import hu.esamu.rft.esamurft.protos.EsamuRFTMessages;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class Main {
-    private static final String URL = "http://localhost:8080/esamu";
+    private static final String REGISTER_URL = "http://localhost:8080/esamu/register";
+    private static final String MESSAGE_URL = "http://localhost:8080/esamu/message";
     private static final String IMAGE_EXAMPLE = "images/image.png";
+
+    private static final String USERNAME_KEY = "username";
+
+    private static List<AbstractMap.SimpleEntry> users = new ArrayList<>();
+    private static List<String> messages = new ArrayList<>();
+
+    private static Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    private static Random r = new Random();
 
     public static byte[] imageConverter(String image) throws IOException {
         InputStream is = Main.class.getClassLoader().getResourceAsStream(image);
-
         return IOUtils.toByteArray(is);
     }
 
     public static void main(String[] args) throws IOException {
+
+        URL messageUrl = new URL(MESSAGE_URL);
+        URL registerUrl = new URL(REGISTER_URL);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number of usernames: ");
+        int numberOfUsers = Integer.parseInt(scanner.next());
+        System.out.println("Number of messages: ");
+        int numberOfMessages = Integer.parseInt(scanner.next());
+        for (int i = 0; i < numberOfUsers; i++) {
+            String username = RandomStringUtils.random(r.nextInt(15), true, true);
+            String id = registerUser((HttpURLConnection) registerUrl.openConnection(), username);
+            LOG.info("User id: " + id);
+            LOG.info("------------" + (i + 1) + ".------------");
+            users.add(new AbstractMap.SimpleEntry(id, username));
+        }
+
+        for (int i = 0; i < numberOfMessages; i++) {
+            sendMessage((HttpURLConnection) messageUrl.openConnection(), r.nextInt(numberOfUsers));
+        }
+    }
+
+    private static String registerUser(HttpURLConnection connection, String user) throws IOException {
+        connection.setRequestMethod("POST");
+
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("charset", "utf-8");
+        connection.setDoOutput(true);
+
+        LOG.info("Sending 'POST' request to URL : " + REGISTER_URL);
+        String param = USERNAME_KEY + "=" + user;
+
+        try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+            wr.write(param.getBytes(StandardCharsets.UTF_8));
+        }
+
+        int responseCode = connection.getResponseCode();
+
+        LOG.info("Post parameters: " + param);
+        LOG.info("Response code: " + responseCode);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuffer result = new StringBuffer();
+        String line;
+        while ((line = br.readLine()) != null) {
+            result.append(line);
+        }
+
+        return result.toString();
+    }
+
+    private static void sendMessage(HttpURLConnection connection, int userIndex) throws IOException {
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/octet-stream");
+
         ByteString bs = ByteString.copyFrom(imageConverter(IMAGE_EXAMPLE));
+        Instant time = Instant.now();
+        Timestamp timestamp = Timestamp.newBuilder().setSeconds(time.getEpochSecond())
+                .setNanos(time.getNano()).build();
+        AbstractMap.SimpleEntry user = users.get(userIndex);
+        EsamuRFTMessages.item message = EsamuRFTMessages.item.newBuilder().setId((String) user.getKey()).setName((String) user.getValue()).setLatitude(0).setLongitude(0).setDescription("Description").setTimestamp(timestamp.getSeconds()).setImage(bs).build();
 
-        URL obj = new URL(URL);
+        connection.setDoOutput(true);
 
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/octet-stream");
+        LOG.info("Sending 'POST' request to URL : " + MESSAGE_URL);
 
-        EsamuRFTMessages.item message = EsamuRFTMessages.item.newBuilder().setName("soma").setLatitude(1).setLongitude(2).setDescription("asd").setTimestamp(234).setImage(bs).build();
+        try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+            wr.write(message.toByteArray());
+        }
 
-        con.setDoOutput(true);
+        int responseCode = connection.getResponseCode();
 
-        System.out.println("\nSending 'POST' request to URL : " + URL);
-
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.write(message.toByteArray());
-        wr.flush();
-        wr.close();
-
-        int responseCode = con.getResponseCode();
-
-        System.out.println("Post parameters : \n" + message.toString());
-        System.out.println("Response Code : " + responseCode);
+        LOG.info("Post parameters : \n" + message.toString());
+        LOG.info("Response Code : " + responseCode);
     }
 }
